267237c071788a1e6dc99fac933d56c99cc59f11
Sergey Cherednichenko <sergey.i.che@gmail.com>
2019-12-31 17:41:30 +0300

    save and get purchase

--- www/src/Controller/PurchaseController.php
+++ www/src/Controller/PurchaseController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Document\Purchase;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ODM\MongoDB\DocumentManager as DocumentManager;
 
@@ -13,23 +14,37 @@ class PurchaseController
     /**
      * @Route("/purchase/{id}", name="get_purchase")
      */
-    public function show(int $id)
+
+    public function __construct (DocumentManager $dm)
     {
-        return new JsonResponse(['items' => [$id]]);
+        $this->dm = $dm;
+    }
+    /**
+     * @Route("/purchase/{id}", name="get_purchase")
+     */
+    public function show(string $id)
+    {
+        $purchase = $this->dm->find(Purchase::class, $id);
+
+        var_dump($purchase);
+        return new JsonResponse(['data' => $purchase]);
     }
 
     /**
      * @Route("/purchase", name="save_purchase", methods={"POST"})
      */
-    public function store(DocumentManager $dm)
+    public function store(Request $request)
     {
 
+        $data = json_decode($request->getContent());
+
+        $receipt = $data->document->receipt;
         $purchase = new Purchase();
-        $purchase->email = "vincent@vfac.fr";
-        $purchase->user ="Винсент";
-        $purchase->inn = "2323324234";
-        $dm->persist($purchase);
-        $dm->flush();
+        $purchase->user = $receipt->user;
+        $purchase->userInn = $receipt->userInn;
+        $purchase->userInn = $receipt->userInn;
+        $this->dm->persist($purchase);
+        $this->dm->flush();
 
         return new JsonResponse(['success' => true, 'purchase_id' => $purchase->id]);
     }
